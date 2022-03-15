ca8e2723ce229fbf2646fc76b447e118d5f34bc2
Scope0204 <scope0204@gmail.com>
2022-03-15 14:22:01 +0900

    Hello Servlet

--- src/main/java/hello/servlet/ServletApplication.java
+++ src/main/java/hello/servlet/ServletApplication.java
@@ -2,10 +2,11 @@ package hello.servlet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
+@ServletComponentScan
 @SpringBootApplication
 public class ServletApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ServletApplication.class, args);
 	}
---
+++ src/main/java/hello/servlet/basic/HelloServlet.java
@@ -0,0 +1,29 @@
+package hello.servlet.basic;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet( name = "helloServlet", urlPatterns = "/hello")
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        System.out.println("HelloServlet.service");
+        System.out.println("request = " + request);
+        System.out.println("response = " + response);
+
+        String username = request.getParameter("username");
+        System.out.println("username = " + username);
+
+        // Content-type 헤더정보
+        response.setContentType("text/plain");
+        response.setCharacterEncoding("utf-8"); // 문자 인코딩
+        // http 메시지 바디
+        response.getWriter().write("hello " + username );
+    }
+
+}
